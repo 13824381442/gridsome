8c8435efebcf628e42e01db4c6f1c923668fd10a
13824381442 <603436653@qq.com>
2021-04-13 22:52:09 +0800

    提交

--- gridsome.config.js
+++ gridsome.config.js
@@ -11,17 +11,7 @@ module.exports = {
       use: '@gridsome/source-strapi',
       options: {
         apiURL: process.env.GRIDSOME_API_URL,
-        // apiURL: 'http://117.50.35.182:1337',
-        // apiURL: 'http://localhost:1337',
-        queryLimit: 1000, // Defaults to 100
         contentTypes: ['post', 'tag'],
-        // singleTypes: ['maker'],
-        // Possibility to login with a Strapi user,
-        // when content types are not publicly available (optional).
-        // loginData: {
-        //   identifier: '',
-        //   password: ''
-        // }
       }
     }
   ],
